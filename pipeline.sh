e1f2f2afe843b8385b6bcfd9eec2c38a76241bdd
Filippo Nicolini <filippo.nicolini6@unibo.it>
2023-09-12 16:07:00 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -11,3 +11,17 @@ mkdir 01_rawreads
 # download the .sra files
 prefetch --option-file 01_rawreads/readsToDownaload.ls -O 01_rawreads/ &
 
+# download .fastq files
+for i in 01_rawreads/SRR28*/*sra; do
+
+    DIR="${i%SRR*}"
+
+    fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files "$i" -O $DIR &&
+    
+    # execute quality check
+    fastqc "$DIR"/*fastq -o "$DIR" -f fastq &&
+
+    # gzip .fastq files
+    gzip -9 "$DIR"/*fastq &&
+    rm $i
+done
