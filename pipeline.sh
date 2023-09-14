8960c4cf1526f55490a2495533becd3144aa4856
Filippo Nicolini <filippo.nicolini6@unibo.it>
2023-09-14 15:03:02 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -86,17 +86,56 @@ bowtie2-build 03_mappings/01_rphi_transcriptome/Rphi.cds.fna 03_mappings/01_rphi
 
 for i in 02_trimmed_reads/SRR*; do
 
+    RPHI="03_mappings/01_rphi_transcriptome/Rphi.cds.fna"
     tmp="${i#*/}" && ACC="${tmp%_*}" &&
 
-    bowtie2 -x 03_mappings/01_rphi_transcriptome/Rphi.cds.fna \
+    # map reads
+    bowtie2 -x $RPHI \
     -1 "$i"/"$ACC"_1_paired.fastq.gz -2 "$i"/"$ACC"_2_paired.fastq.gz \
-    -S 03_mappings/"$ACC".mapped.bt2.sam --no-discordant -p 30 \
-    2> 03_mappings/"$ACC".mapped.bt2.log &&
+    -S 03_mappings/"$ACC".mapped.sam --no-discordant -p 30 \
+    2> 03_mappings/"$ACC".mapped.log &&
     
-    samtools view -b 03_mappings/"$ACC".mapped.bt2.sam > 03_mappings/"$ACC".mapped.bt2.bam &&
+    # conert sam to bam
+    samtools view -b 03_mappings/"$ACC".mapped.sam > 03_mappings/"$ACC".mapped.bam &&
     
-    rm 03_mappings/"$ACC".mapped.bt2.sam &&
+    # remove sam
+    rm 03_mappings/"$ACC".mapped.sam &&
+
+    # sort and filter bam file
+    samtools sort -@ 30 03_mappings/"$ACC".mapped.bam |\
+    samtools view -t $RPHI -F 4 -h -@ 30 -b > 03_mappings/"$ACC".mapped.sorted.filtered.bam &&
+
+    # get raw counts statistics
+    samtools index 03_mappings/"$ACC".mapped.sorted.filtered.bam &&
+    samtools idxstats 03_mappings/"$ACC".mapped.sorted.filtered.bam > 03_mappings/"$ACC".rawmapping.stats.tsv &&
 
     echo "$ACC": OK
     
 done
+
+# merge all raw mapping files into one 
+for i in 03_mappings/SRR28*tsv; do
+
+    TMP="03_mappings/TMP"
+
+    if [ ! -e $TMP ]; then
+
+        awk '{print $1"\t"$3}' $i | head -n -1 > $TMP
+        continue
+
+    else
+
+        if [ -e 03_mappings/ALL.rawmapping.stats.tsv ]; then
+
+            rm 03_mappings/ALL.rawmapping.stats.tsv
+
+        fi
+
+        awk '{print $1"\t"$3}' $i | head -n -1 | join -j 1 -t $'\t' $TMP - > 03_mappings/ALL.rawmapping.stats.tsv &&
+        cp 03_mappings/ALL.rawmapping.stats.tsv $TMP
+
+    fi
+
+done
+
+rm 03_mappings/TMP
